3a974ef2f74fe2d6f7e031465dfb1cb04a5b0fab
baoanh1310 <dataholicguy@gmail.com>
2021-05-20 19:03:44 +0700

    Connect firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDxfjLwRE3zFzaNn1Ji7Jb3MBgfvmIXQvU",
+    authDomain: "itss-todolist.firebaseapp.com",
+    projectId: "itss-todolist",
+    storageBucket: "itss-todolist.appspot.com",
+    messagingSenderId: "630815441503",
+    appId: "1:630815441503:web:3955f36c1f5eedc764d1ea",
+    measurementId: "G-C3M17KQ0NJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
